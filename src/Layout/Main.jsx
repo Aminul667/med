c01d4016822e25c99e13082fbfe3a87bbba343e1
Aminul667 <rahathssn@gmail.com>
2024-02-12 11:32:09 +0600

    added daisyUI

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,16 @@
+const Main = () => {
+  return (
+    <div>
+      <h1 className="p-5 bg-slate-100">Hello React Router</h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+    </div>
+  );
+};
+
+export default Main;
