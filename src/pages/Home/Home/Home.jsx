1c2287866e605749fdacbdb833f1c89eb05b2de3
Aminul667 <rahathssn@gmail.com>
2024-02-12 15:38:30 +0600

    added position

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -4,17 +4,17 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="banner-background px-40 py-52 h-[100vh]">
-      <div className="glass px-10 py-20 rounded-xl">
-        <h2 className="text-5xl font-semibold text-cyan-500 mb-5">
+      <div className="glass px-10 py-20 rounded-xl relative">
+        <h2 className="text-5xl font-semibold text-cyan-500">
           Real-time Medication for your family
         </h2>
-        <p className="text-3xl text-cyan-200">
+        <p className="text-3xl text-cyan-200 my-5">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
           pariatur provident at deleniti earum incidunt voluptate aut? Dicta,
           laboriosam omnis cum hic aliquam impedit voluptas voluptates, esse
           tempora voluptatum quasi.
         </p>
-        <button className="text-red-600">Get Start</button>
+        <button className="text-cyan-500 text-3xl px-4 py-2 border rounded-md hover:bg-cyan-200 absolute right-4">Get Started</button>
       </div>
       {/* <img src={banner} alt="" /> */}
     </div>
