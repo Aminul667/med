819e881d85099bbc99227027d03374201cc668d9
Aminul667 <rahathssn@gmail.com>
2024-02-12 15:23:54 +0600

    added banner design

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -3,18 +3,18 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <div className="banner-background px-5 py-52">
-        <h2 className="text-5xl font-semibold">
+    <div className="banner-background px-40 py-52 h-[100vh]">
+      <div className="glass px-10 py-20 rounded-xl">
+        <h2 className="text-5xl font-semibold text-cyan-500 mb-5">
           Real-time Medication for your family
         </h2>
-        <p className="text-3xl">
+        <p className="text-3xl text-cyan-200">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
           pariatur provident at deleniti earum incidunt voluptate aut? Dicta,
           laboriosam omnis cum hic aliquam impedit voluptas voluptates, esse
           tempora voluptatum quasi.
         </p>
-        <button>Get Start</button>
+        <button className="text-red-600">Get Start</button>
       </div>
       {/* <img src={banner} alt="" /> */}
     </div>
