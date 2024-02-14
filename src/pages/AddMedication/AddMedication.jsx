c510fdcbe33677a24556e941825588bcb4f04f48
Aminul667 <rahathssn@gmail.com>
2024-02-14 12:49:14 +0600

    added labels on the top

--- src/pages/AddMedication/AddMedication.jsx
+++ src/pages/AddMedication/AddMedication.jsx
@@ -5,7 +5,7 @@ const AddMedication = () => {
     register,
     handleSubmit,
     formState: { errors },
-    control
+    control,
   } = useForm();
   const { fields, append, remove } = useFieldArray({
     control: control,
@@ -37,32 +37,34 @@ const AddMedication = () => {
         />
       </div>
       {fields.map((field, index) => (
-        <div key={field.id} className="mb-4 flex items-center">
+        <div key={field.id} className="mb-4">
           <label
+            className="block text-gray-700 text-sm font-bold mb-2"
             htmlFor={`firstName${index}`}
-            className="block text-gray-700 text-sm font-bold mb-2 mr-2"
           >
             First Name:
           </label>
-          <input
-            type="text"
-            {...register(`inputFields.${index}.firstName`, {
-              required: "First Name is required",
-            })}
-            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2"
-          />
-          {errors?.inputFields?.[index]?.firstName && (
-            <p className="text-red-500 text-xs mt-1">
-              {errors.inputFields[index].firstName.message}
-            </p>
-          )}
-          <button
-            type="button"
-            onClick={() => remove(index)}
-            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Delete
-          </button>
+          <div className="flex items-center">
+            <input
+              type="text"
+              {...register(`inputFields.${index}.firstName`, {
+                required: "First Name is required",
+              })}
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2"
+            />
+            {errors?.inputFields?.[index]?.firstName && (
+              <p className="text-red-500 text-xs mt-1">
+                {errors.inputFields[index].firstName.message}
+              </p>
+            )}
+            <button
+              type="button"
+              onClick={() => remove(index)}
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Delete
+            </button>
+          </div>
         </div>
       ))}
       <button
