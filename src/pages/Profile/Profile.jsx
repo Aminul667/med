b64d795fa5d9b83d0bfef2d45b975cc6505a8bd5
Aminul667 <rahathssn@gmail.com>
2024-02-19 10:43:25 +0600

    added card component"

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,26 +1,13 @@
 import { useLoaderData } from "react-router-dom";
+import Card from "./Card";
 
 const Profile = () => {
   const prescriptionData = useLoaderData();
   console.log(prescriptionData);
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-gradient-to-br from-blue-500 to-purple-500 bg-opacity-75 backdrop-blur-md backdrop-filter">
-      <div className="px-6 py-4">
-        <div className="font-semibold text-xl mb-2 text-white">Card Title</div>
-        <p className="text-gray-200 text-base">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
-          quam sed massa sollicitudin varius.
-        </p>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag2
-        </span>
-      </div>
+    <div className="m-5">
+      <Card></Card>
     </div>
   );
 };
